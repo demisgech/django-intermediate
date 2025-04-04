4051413337e09a119e0208dc9e51f5460b506fe4
Demis Getachew <demisgech@gmail.com>
2025-04-04 11:05:32 +0300

    Setup project dependencies for for django api designing part

--- store/views.py
+++ store/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+# from django.http import HttpResponse, HttpRequest
 
-# Create your views here.
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework.request import Request
+
+# Built in
+# def product_list(request:HttpRequest)->HttpResponse:
+#     return HttpResponse("OK.")
+
+
+# Restframework
+@api_view()
+def product_list(request: Request) -> Response:
+    return Response("Ok.")
+    
+
+@api_view()
+def product_detail(request:Request,id:int) -> Response:
+    return Response(id)
+    
