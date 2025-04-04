d449bdb262a91b2a92f84d05f311c5304ed2e299
Demis Getachew <demisgech@gmail.com>
2025-04-04 11:03:11 +0300

    register polls in the admin site

--- polls/admin.py
+++ polls/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
 
+from .models import Category, Choice, Comment, Question
+
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    
+    
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = [
+        'question_text',
+        'published_date',
+        'category',
+    ]
+    
+    date_hierarchy = 'published_date'
+    inlines = [ChoiceInline]
+    list_filter = ['published_date', 'category']
+    ordering = ['published_date']
+    search_fields = ['question_text']
+    
+    
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ['user','text','created_at']
+    list_filter = ['created_at','user']
+    search_fields = ['text']
+    date_hierarchy = 'created_at'
+    ordering = ['created_at']
+    
